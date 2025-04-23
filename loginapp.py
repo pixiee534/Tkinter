4157e3961934a978f22c7422028aa993ac360463
pixiee534 <pixiee345@gmail.com>
2025-04-23 17:45:35 +0530

    tkinter

---
+++ loginapp.py
@@ -0,0 +1,27 @@
+root = Tk()
+root.title('Login App')
+root.geometry('400x400')
+frame = Frame (master-root, height=200, width=360, bg="#d0efff")
+lb11 =Label (frame, text="Full Name", bg="#3895D3", fg='white', width=12) 
+lb12 =Label (frame, text="Email Id", bg="#3895D3", fg='white', width=12) 
+lb13= Label (frame, text="Enter Password", bg="#3895D3", fg='white', width=12)
+name_entry=Entry (frame)
+email_entry=Entry (frame)
+pass_entry =Entry (frame, show="*")
+# Function to display message
+def display():
+    name =name_entry.get()
+    greet= "Hey " + name
+    message ="\nCongratulations for your new account!" 
+    textbox.insert(END, greet)
+    textbox.insert(END, message)
+textbox= Text (bg="#BEBEBE", fg="black")
+btn = Button(text="Create Account", command-display, bg="red")
+frame.place(x=20, y=0)
+lb11.place(x=20, y=20)
+name_entry.place (x-150, y=20)
+lb12.place(x=20, y=80)
+email_entry.place(x=150, y=80) 1b13.place(x=20, y=140)
+pass_entry.place (x=150, y=140) btn.place(x=130, y=210) 
+textbox.place(y=250) 
+root.mainloop()
